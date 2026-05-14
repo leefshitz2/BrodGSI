2773b4649b7b31da260283b0c7db97257968f6ea
RofikKernelDev <rofikcoga@gmail.com>
2026-05-14 19:21:34 +0700

    Refactor product directory selection in make.sh

--- Patches/17/make.sh
+++ Patches/17/make.sh
@@ -3,10 +3,14 @@
 BASE_DIR=$1
 SCRIPT_DIR=$(dirname "$0")
 
-if [ -d "$BASE_DIR/product" ] && [ ! -L "$BASE_DIR/product" ]; then
-    product="$BASE_DIR/product"
-elif [ -d "$BASE_DIR/system/product" ] && [ ! -L "$BASE_DIR/system/product" ]; then
+# Prefer PHH/SAR layout first
+if [ -d "$BASE_DIR/system/product" ] && [ ! -L "$BASE_DIR/system/product" ]; then
     product="$BASE_DIR/system/product"
+
+# Fallback for old layouts
+elif [ -d "$BASE_DIR/product" ] && [ ! -L "$BASE_DIR/product" ]; then
+    product="$BASE_DIR/product"
+
 else
     echo "error: No product dir"
     exit 1
