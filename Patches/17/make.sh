3237cc7a14f191c495ec448a46f41cecd5cea789
RofikKernelDev <rofikcoga@gmail.com>
2026-05-15 08:20:03 +0700

    Update make.sh

--- Patches/17/make.sh
+++ Patches/17/make.sh
@@ -3,16 +3,10 @@
 BASE_DIR=$1
 SCRIPT_DIR=$(dirname "$0")
 
-# Prefer PHH/SAR layout first
-if [ -d "$BASE_DIR/system/product" ] && [ ! -L "$BASE_DIR/system/product" ]; then
-    product="$BASE_DIR/system/product"
+product="$BASE_DIR/system/product"
 
-# Fallback for old layouts
-elif [ -d "$BASE_DIR/product" ] && [ ! -L "$BASE_DIR/product" ]; then
-    product="$BASE_DIR/product"
-
-else
-    echo "error: No product dir"
+if [ ! -d "$product" ]; then
+    echo "error: Missing PHH product dir"
     exit 1
 fi
 
