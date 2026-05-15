ef8fd50eb12280055bea3a93f4916adfaacdf0d7
RofikKernelDev <rofikcoga@gmail.com>
2026-05-15 08:20:50 +0700

    Update make.sh

--- Patches/15/make.sh
+++ Patches/15/make.sh
@@ -3,18 +3,9 @@
 BASE_DIR=$1
 SCRIPT_DIR=$(dirname "$0")
 
-# Prefer PHH/SAR layout first
-if [ -d "$BASE_DIR/system/product" ] && [ ! -L "$BASE_DIR/system/product" ]; then
+
     product="$BASE_DIR/system/product"
 
-# Fallback for old layouts
-elif [ -d "$BASE_DIR/product" ] && [ ! -L "$BASE_DIR/product" ]; then
-    product="$BASE_DIR/product"
-
-else
-    echo "error: No product dir"
-    exit 1
-fi
 
 # PHH Patches
 rsync -ra "$SCRIPT_DIR/system/" "$BASE_DIR/system/"
